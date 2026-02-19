35cde718344e2007e39d311e9ea7e10db0722292
parthmangalekar <parthmangalekar2004@gmail.com>
2026-02-19 09:36:54 +0530

    second commit

--- 2fa-auth-gen.py
+++ 2fa-auth-gen.py
@@ -5,4 +5,16 @@ key = "Specialkey"
 
 totp = pyotp.TOTP(key)
 
+
 print(totp.now())
+
+input_code= input("Enter 2FA Code: ")
+
+code= totp.now()
+print(input_code)
+
+if (input_code== code):
+    print('True')
+
+else:
+    print('False')
