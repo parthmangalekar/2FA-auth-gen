9cf9a905422a6f17e9d478468f488794fb384d4b
parthmangalekar <parthmangalekar2004@gmail.com>
2026-02-21 10:03:50 +0530

    fifth commit

--- authenticator.py
+++ authenticator.py
@@ -2,20 +2,23 @@ import pyotp
 import time
 import sys
 
-key= pyotp.random_base32
+key= pyotp.random_base32()
 totp = pyotp.TOTP(key)
 
 print ('Secret key: ', (key))
 
 try:
-    while true:
+    while True:
         current_code= totp.now()
 
         time_remaining= int(30 -(time.time() % 30))
 
         bar = "/" * (30 - time_remaining) + "-" * time_remaining
 
-        sys.stdout.write (f"/rTOTP Code: {current_code} | [{bar}] {time_remaining}") sys.stdout.flush()
+        sys.stdout.write (f"/rTOTP Code: {current_code} | [{bar}] {time_remaining}")
+        sys.stdout.flush()
 
         time.sleep(1)
-        
+
+except KeyboardInterrupt:
+    print("\nStopped.")
