da07e8b5da8d837ee02780ccc0f14ae68d3d81d7
parthmangalekar <parthmangalekar2004@gmail.com>
2026-02-24 16:49:15 +0530

    new lib for future commits

--- authenticator.py
+++ authenticator.py
@@ -1,5 +1,8 @@
 import pyotp
 import time
+import cv2
+import os
+import pathlib
 
 print ('-----Authenticator setup-----')
 print('Select a method: ')
@@ -10,7 +13,6 @@ print("3. Enter Secret Key")
 choice = (input('Select a method: '))
 
 if choice =='1':
-    print('Qr code detection code is yet to be implemented. check back later :D')
 
 elif choice =='2':
    url= (input('Paste your link here: '))
